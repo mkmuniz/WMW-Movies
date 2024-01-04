be4b75cf11b58bfd3292afc79b19b508e15da03c
Mikael <mikaelmuniz2001@gmail.com>
2024-01-04 12:17:15 -0300

    fix: sliders

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -7,18 +7,27 @@ import { Movie } from "types/movie";
 import { SliderProps } from "types/props";
 
 const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 2560 },
+    items: 7,
+  },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    slidesToSlide: 3,
+    breakpoint: { max: 2560, min: 1024 },
+    items: 5,
+    slidesToSlide: 5,
   },
   tablet: {
     breakpoint: { max: 1024, min: 768 },
+    items: 3,
+    slidesToSlide: 3,
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 400 },
     items: 2,
     slidesToSlide: 2,
   },
-  mobile: {
-    breakpoint: { max: 767, min: 280 },
+  smallMobile: {
+    breakpoint: { max: 400, min: 150 },
     items: 1,
     slidesToSlide: 1,
   },
